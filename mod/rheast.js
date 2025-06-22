78d37973ae0e5c54c59c3258f5ce5576f2803774
rheast <chen@rheast.com>
2025-06-23 00:14:41 +0800

    Add files via upload

---
+++ mod/rheast.js
@@ -0,0 +1,175 @@
+// StaticRender - Rheast.js
+
+class RHEast {
+    constructor() {
+        this.name = 'StaticRender';
+        this.time = 'Jan 15, 2025';
+        this.version = `v${1.08}`;
+        this.virtual = [];
+        this.forStyle();
+        console.log(this.name, this.version, 'Rheast.js');
+        document.addEventListener('DOMContentLoaded', () => {
+            this.forRender();
+            this.forLoad();
+        });
+    }
+
+    forLoad() {
+        Array.from(document.querySelectorAll('[_data]')).forEach(div => {
+            fetch(div.getAttribute('_data')).then(response => {
+                return response.json();
+            }).then(data => {
+                let handle = div.getAttribute('_handle');
+                if (handle && typeof window[handle] === 'function') {
+                    data = window[handle](data) || data;
+                }
+                this.forRender(div, data);
+                Array.from(document.querySelectorAll('[_load]')).forEach(item => {
+                    this.forComponent(item, data);
+                });
+            }).catch(error => console.error(error));
+        });
+    }
+
+    forComponent(div, data) {
+        div = this.div(div);
+        fetch(div.getAttribute('_load')).then(response => {
+            return response.text();
+        }).then(text => {
+            div.innerHTML = text;
+            this.forRender(div, data);
+            div.removeAttribute('_load');
+        }).catch(error => console.error(error));
+    }
+
+    forRender(div, data) {
+        div = this.div(div);
+        div = div || document.querySelector('body');
+        let box = Array.from(div.querySelectorAll('[_for]')).filter(
+            element => !Array.from(div.querySelectorAll('[_for] [_for],[_bind] [_for]')).includes(element)
+        );
+        Array.from(box).forEach(item => { this.forClone(item, data) });
+    }
+
+    forClone(div, data) {
+        div = this.div(div);
+        switch (true) {
+            case !div: {
+                return false;
+            }
+            case div.hasAttribute('_for'): {
+                return this.forLoop(div, data);
+            }
+        }
+    }
+
+    forLoop(div, data) {
+        data = this.forData(div, data);
+        if (data && Array.isArray(data)) {
+            data.forEach((item, index) => {
+                let clone = div.cloneNode(true);
+                clone.removeAttribute('_for');
+                this.forReplace(clone, this.forValue(item, index));
+                this.forRetard(clone);
+                if (!clone.hasAttribute('_retard')) {
+                    div.parentNode.insertBefore(clone, div);
+                }
+            });
+            div.remove();
+        }
+    }
+
+    forData(div, data) {
+        try {
+            const target = div.getAttribute('_for');
+            switch (true) {
+                case !target:
+                    return data;
+                case /^\[.*\]$|^\{.*\}$/.test(target):
+                    return JSON.parse(target.replaceAll("'", '"'));
+                case data && target in data:
+                    return data[target];
+                case Array.isArray(data):
+                    return data;
+                case target in window:
+                    return window[target];
+                default:
+                    return data;
+            }
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+
+    forValue(data, index) {
+        data = typeof data === 'object' ? data : { _this: data, _value: data };
+        data._index = index;
+        return data;
+    }
+
+    forReplace(div, data) {
+        Array.from(div.attributes).forEach(attr => {
+            switch (true) {
+                case ['_for', '_bind'].includes(attr.name): {
+                    break;
+                }
+                case ['_if', '_else'].includes(attr.name): {
+                    let value = data[attr.value];
+                    value = (typeof value === 'number' || value);
+                    if ((attr.name === '_if' && value) || (attr.name === '_else' && !value)) {
+                        div.removeAttribute(attr.name);
+                    } else {
+                        div.setAttribute('_retard', true);
+                    }
+                    break;
+                }
+                default: {
+                    attr.value = this.forBrackets(attr.value, data);
+                    if (attr.name.startsWith('_')) {
+                        let name = attr.name.slice(1);
+                        let value = div.getAttribute(name) || '';
+                        div.setAttribute(name, value + attr.value);
+                        div.removeAttribute(attr.name);
+                    }
+                    break;
+                }
+            }
+        });
+        Array.from(div.childNodes).forEach(node => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                node.nodeValue = this.forBrackets(node.nodeValue, data);
+            } else if (node.nodeType === Node.ELEMENT_NODE) {
+                node.getAttribute('_for') ? this.forClone(node, data) : this.forReplace(node, data);
+            }
+        });
+    }
+
+    forRetard(div) {
+        div.querySelectorAll('[_retard]').forEach(item => item.remove());
+    }
+
+    forBrackets(value, data) {
+        return value.replace(/{(.*?)}/g, (_, key) => data[key] === 0 ? '0' : (data[key] ? String(data[key]) : ''));
+    }
+
+    forStyle() {
+        let style = document.createElement('style');
+        style.textContent = '[_for]{visibility:hidden;}';
+        document.querySelector('head').appendChild(style);
+    }
+
+    div(div) {
+        return typeof div === 'string' ? document.querySelector(div) : div;
+    }
+
+    href(name) {
+        return new URL(window.location.href).searchParams.get(name);
+    }
+
+    resize() {
+        window.addEventListener('resize', () => location.reload());
+    }
+}
+
+const rheast = new RHEast();
---
+++ mod/tailwind.py
@@ -0,0 +1,149 @@
+import os
+
+
+def setBase(data, z=False):
+    for index in data:
+        index = zip(*index) if z else [index]
+        for name, style, value, cut in index:
+            value = value if value else name
+            cut = name[:cut] if type(cut) == type(1) else cut
+            cut = [cut] if type(cut) == type("") else cut
+            cut = ", ".join(f".{x}" for x in cut) if type(cut) == type([]) else cut
+            text = f".{name} {bracket.replace('*', f' {style}: {value}; ')}"
+            text = f"{cut}, {text}" if type(cut) == type("") else text
+            matrix.append(text)
+    return
+
+
+def setNumber(e, a, b, s, v):
+    cut = [f"{e[0]}{n[0]}-{s}" for n in [a, b]]
+    info.append([f"{e[0]}-{s}", f"{e}-{a}", v, cut])
+
+
+matrix = []
+bracket = "{*}"
+matrix += ["/* Base */"]
+direct = ["left", "right", "top", "bottom"]
+info = [
+    [["relative", "absolute", "fixed"], ["position"] * 3, [False] * 3, [3] * 3],
+    [["multiply", "darken", "lighten"], ["mix-blend-mode"] * 3, [False] * 3, [3, 4, 5]],
+    [["bold", "normal"], ["font-weight"] * 2, [False] * 2, [False] * 2],
+    [direct] * 2 + [["0"] * 4, [False] * 4],
+    [["inline", "block", "inline-block"], ["display"] * 3, [False] * 3, [False] * 3],
+    [["none"], ["display"], [False], [False]],
+    [["of-hidden"], ["overflow"], ["hidden"], [["hide", "over-hide"]]],
+    [["ws-nowrap"], ["white-space"], ["nowrap"], ["nowrap"]],
+]
+
+setBase(info, True)
+info = []
+
+for e in ["left", "right", "center", "justify"]:
+    info.append([f"text-{e}", "text-align", e, f"t-{e[0]}t"])
+
+img = [
+    "background-position: center",
+    "background-repeat: no-repeat",
+    "background-size: cover",
+]
+img = "".join(f" {x};" for x in img) + " "
+matrix += [f".img, .image {bracket.replace('*', img)}"]
+matrix += [".img { background-size: contain; }"]
+
+setBase(info)
+matrix += ["", "/* Flex */"]
+flex, info = ["flex-center"], []
+inside = [
+    ["justify-content", ["center"], "", False],
+    ["justify-content", ["start", "end"], "flex", False],
+    ["justify-content", ["evenly", "between", "around"], "space", False],
+    ["align-items", ["center", "start", "end", "stretch"], "", False],
+    ["flex-direction", ["row", "column"], "", 3],
+    ["flex-wrap", ["wrap", "nowrap"], "", False],
+]
+
+for style, index, before, cut in inside:
+    before = f"{before}-" if before else ""
+    for i in index:
+        name = i[:cut] if cut else i
+        name = f"{style.split('-')[0]}-{name}"
+        after = style.split("-")
+        after = [] if "flex" in after else [f"{x}-{i}" for x in after]
+        after = [x for x in after if x != name]
+        if i == "center":
+            after.append("flex-center")
+        flex += [name, *after]
+        info.append([name, style, f"{before}{i}", after if len(after) else False])
+
+info.append(["flex", "display", "flex", flex])
+setBase(info)
+matrix += ["", "/* Width & Height */"]
+info = []
+
+for e in ["width", "height"]:
+    for a, b, c in ([e[:1], "%", "wh"], [f"v{e[:1]}"] * 2 + ["v"]):
+        for i in [25, 33.33, 50, 75, 100]:
+            text, cut = f"{a}-{int(i)}", False
+            if i == 100:
+                text, cut = f"{a}-full", f"{c}-full"
+            info.append([text, e, f"{i}{b}", cut])
+
+    for a, b, c in (["", "px", 1], ["e", "em", 9]):
+        for i in range(c):
+            text, cut = f"{e[:1]}-{int(i+1)}{a}", f"wh-{int(i+1)}{a}"
+            info.append([text, e, f"{i+1}{b}", cut])
+
+    for i in range(9):
+        info.append([f"m{e[0]}-{i+1}e", f"min-{e}", f"{i+1}em", f"m-{i+1}e"])
+
+
+setBase(info)
+matrix += ["", "/* Padding & Margin */"]
+info = []
+
+for e in ["padding", "margin"]:
+    for a, b in zip(direct, ["x", "x", "y", "y"]):
+        setNumber(e, a, b, "a", "auto")
+        for i in range(21):
+            if i < 9 or i % 4 == 0:
+                setNumber(e, a, b, i, f"{i/4}em")
+                if e == "margin" and i > 0:
+                    setNumber(e, a, b, f"{i}c", f"-{i/4}em")
+
+setBase(info)
+matrix += ["", "/* Font Size */"]
+info = []
+
+for e in ["s*", "x*", "*e", "i*"]:
+    for i in range(7):
+        a = e.replace("*", f"{i+1}")
+        b = (i + 12) / 16 if e[0] == "s" else (i + 9) / 8 if e[0] == "x" else i + 1
+        c = "text-indent" if e[0] == "i" else "font-size"
+        if e[0] == "x" or i < 5:
+            info.append([f"text-{a}", c, f"{b}em", f"t-{a}"])
+
+setBase(info)
+matrix += ["", "/* Other */"]
+matrix += [".border { border-width: 1px; border-style: solid; }"]
+matrix += [".letter { text-indent: 1px; letter-spacing: 1px }"]
+info = []
+
+for a, b in [["o", "opacity"], ["z", "z-index"], ["r", "border-radius"]]:
+    e = b.split("-")[-1]
+    for i in range(10):
+        num = f"{i/4}em" if a == "r" else str(i)
+        if a == "o":
+            num = f"{i*25}%" if i < 5 else False
+        if num:
+            info.append([f"{e}-{i}", b, num, f"{a}-{i}"])
+    if a == "r":
+        for i, j in [["max", "10em"], ["full", "100%"]]:
+            info.append([f"{e}-{i}", b, j, [f"{a}-{i[0]}", f"{a}-{i}"]])
+
+setBase(info)
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, f"tailwind.css")
+
+with open(path, "w", encoding="utf-8") as f:
+    for i in matrix:
+        f.write(str(i) + "\n")
